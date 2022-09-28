24e7805df2da68deb1ad933091122db9cff501b0
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-09-28 21:38:35 +0600

    header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
         <div className='header'>
             <h1>IB-Fitness-Club</h1>
+            <div>
+                <a href="/home">Home</a>
+                <a href="/about">About</a>
+            </div>
         </div>
     );
 };
