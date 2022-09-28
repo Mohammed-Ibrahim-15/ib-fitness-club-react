e3117e5e6591f5a95692769a66e6f25458866d5e
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-09-28 21:47:08 +0600

    activities updated

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Activities.css'
 
 const Activities = () => {
+    const [activities, setActivities] = useState([])
+    useEffect(() => {
+        fetch('fitness.json')
+            .then(res => res.json())
+            .then(data => setActivities(data))
+    }, [])
     return (
-        <div>
+        <div className='main-container'>
+            <div className="activity-container">
+                <h1>Activity</h1>
+                {
+                    activities.map(activity => <p>{activity.name}</p>)
+                }
 
+            </div>
+            <div className="cart-container">
+                <h1>Cart</h1>
+            </div>
         </div>
     );
 };
