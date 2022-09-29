93e67e16dd5574f165543386aba0b62f2c84e429
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-09-29 15:44:45 +0600

    question component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Activities from './components/Activities/Activities';
+import Question from './components/Question/Question';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Activities></Activities>
+      <Question></Question>
     </div>
   );
 }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Question.css'
+
+const Question = () => {
+    return (
+        <div>
+            <h1>Add Question</h1>
+        </div>
+    );
+};
+
+export default Question;
